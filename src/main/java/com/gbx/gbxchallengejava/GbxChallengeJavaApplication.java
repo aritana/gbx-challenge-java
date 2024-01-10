4655cc861c7e7ab670c733a4da41bdb1c17e0e0f
noara <tutoravacefet@gmail.com>
2024-01-10 12:00:57 -0300

    Organize project structure and add necessary libraries
    - Rearrange folder structure to improve organization
    - Create separate directories for service, controller and repository
    - Integrate essential libraries for enhanced functionalities

---
+++ src/main/java/com/gbx/gbxchallengejava/GbxChallengeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gbx.gbxchallengejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbxChallengeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbxChallengeJavaApplication.class, args);
+	}
+
+}
